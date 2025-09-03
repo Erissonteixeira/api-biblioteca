fbad1e90ec9382e0265c4a865e302fee8bafceb9
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-03 16:00:50 -0300

    feat: criar entidade Livro

---
+++ src/main/java/io/github/Erissonteixeira/api_crudbiblioteca/model/Livro.java
@@ -0,0 +1,80 @@
+package io.github.Erissonteixeira.api_crudbiblioteca.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+@Entity
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String titulo;
+    @Min(value = 1990, message = "O ano de publicação deve ser maior ou igual que 1900")
+    @Max(value = 2025, message = "O ano de publicação não pode ser no futuro")
+    private Integer anoPublicacao;
+    @Column(nullable = false, length = 50)
+    private String genero;
+    @Column(nullable = false)
+    private String status;
+    @ManyToOne
+    @JoinColumn(name = "autor_id", nullable = false)
+    private Autor autor;
+
+    public Livro(){
+    }
+
+    public Livro(String titulo, Integer anoPublicacao, String genero, String status, Autor autor){
+        this.titulo = titulo;
+        this.anoPublicacao = anoPublicacao;
+        this.genero = genero;
+        this.status = status;
+        this.autor = autor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+}
