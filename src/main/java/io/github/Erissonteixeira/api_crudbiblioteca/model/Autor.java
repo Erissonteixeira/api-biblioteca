74a3f9e7865b7b4abac2ba225fc87233a1cef4bf
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-03 14:51:16 -0300

    feat: criar entidade Autor

---
+++ src/main/java/io/github/Erissonteixeira/api_crudbiblioteca/model/Autor.java
@@ -0,0 +1,44 @@
+package io.github.Erissonteixeira.api_crudbiblioteca.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Livro> livros = new ArrayList<>();
+
+    public Autor(){
+    }
+    public Autor(String nome){
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+}
