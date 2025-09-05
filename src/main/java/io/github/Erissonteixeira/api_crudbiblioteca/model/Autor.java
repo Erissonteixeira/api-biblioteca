6ea0435c0bc59f8467e9ea0f1d31505b7e38f8a4
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-05 14:30:20 -0300

    feat: inicializa lista de livros com @Builder.Default na entidade Autor

--- src/main/java/io/github/Erissonteixeira/api_crudbiblioteca/model/Autor.java
+++ src/main/java/io/github/Erissonteixeira/api_crudbiblioteca/model/Autor.java
@@ -3,8 +3,6 @@ package io.github.Erissonteixeira.api_crudbiblioteca.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +17,7 @@ public class Autor {
     @Column(nullable = false)
     private String nome;
     @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
     private List<Livro> livros = new ArrayList<>();
 
     public Autor(){
