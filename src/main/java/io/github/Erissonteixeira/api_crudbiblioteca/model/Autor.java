409a8b0245ea554726486d6cdc1bd011b2f54707
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-05 11:41:04 -0300

    test: adiciona @Builder e @AllArgsConstructor na entidade Autor para suportar criação de objetos em testes

--- src/main/java/io/github/Erissonteixeira/api_crudbiblioteca/model/Autor.java
+++ src/main/java/io/github/Erissonteixeira/api_crudbiblioteca/model/Autor.java
@@ -1,10 +1,16 @@
 package io.github.Erissonteixeira.api_crudbiblioteca.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Builder
+@AllArgsConstructor
 public class Autor {
 
     @Id
