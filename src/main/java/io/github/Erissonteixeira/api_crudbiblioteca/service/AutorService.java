00e584cd9bd38f17c76e03a97f54dd1b19ba1260
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-09-10 16:26:42 -0300

    refactor: refatorar AutorService usando AutorMapper

--- src/main/java/io/github/Erissonteixeira/api_crudbiblioteca/service/AutorService.java
+++ src/main/java/io/github/Erissonteixeira/api_crudbiblioteca/service/AutorService.java
@@ -2,6 +2,7 @@ package io.github.Erissonteixeira.api_crudbiblioteca.service;
 
 import io.github.Erissonteixeira.api_crudbiblioteca.dto.AutorRequestDTO;
 import io.github.Erissonteixeira.api_crudbiblioteca.dto.AutorResponseDTO;
+import io.github.Erissonteixeira.api_crudbiblioteca.mapper.AutorMapper;
 import io.github.Erissonteixeira.api_crudbiblioteca.model.Autor;
 import io.github.Erissonteixeira.api_crudbiblioteca.repository.AutorRepository;
 import org.springframework.stereotype.Service;
@@ -13,30 +14,32 @@ import java.util.stream.Collectors;
 public class AutorService {
 
     private final AutorRepository autorRepository;
+    private final AutorMapper autorMapper;
 
-    public AutorService(AutorRepository autorRepository) {
+    public AutorService(AutorRepository autorRepository, AutorMapper autorMapper) {
         this.autorRepository = autorRepository;
+        this.autorMapper = autorMapper;
     }
 
     public AutorResponseDTO criarAutor(AutorRequestDTO dto){
         if (dto.getNome() == null || dto.getNome().isBlank()){
-            throw new NullPointerException("O Nome do autor não pode ser nulo ou vazio");
+            throw new RuntimeException("O Nome do autor não pode ser nulo ou vazio");
         }
-        Autor autor = new Autor(dto.getNome());
+        Autor autor = autorMapper.toEntity(dto);
         Autor salvo = autorRepository.save(autor);
-        return new AutorResponseDTO(salvo.getId(), salvo.getNome());
+        return autorMapper.toDTO(salvo);
     }
 
     public List<AutorResponseDTO> listarAutores(){
         return autorRepository.findAll()
                 .stream()
-                .map(a -> new AutorResponseDTO(a.getId(), a.getNome()))
+                .map(autorMapper::toDTO)
                 .collect(Collectors.toList());
     }
 
     public Optional<AutorResponseDTO> buscarPorId(Long id){
         return autorRepository.findById(id)
-                .map(a -> new AutorResponseDTO(a.getId(), a.getNome()));
+                .map(autorMapper::toDTO);
     }
 
     public AutorResponseDTO atualizarAutor(Long id, AutorRequestDTO dto){
@@ -44,7 +47,7 @@ public class AutorService {
                 .map(autor -> {
                     autor.setNome(dto.getNome());
                     Autor atualizado = autorRepository.save(autor);
-                    return new AutorResponseDTO(atualizado.getId(), atualizado.getNome());
+                    return autorMapper.toDTO(atualizado);
                 })
                 .orElseThrow(() -> new RuntimeException("Autor não encontrado"));
     }
